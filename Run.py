5c93dd497cc2a6ce961bdf7c1617c50fa27196af
tarom0111 <77558188+tarom0111@users.noreply.github.com>
2024-10-05 19:31:24 +0700

    Add files via upload

---
+++ Run.py
@@ -0,0 +1,11 @@
+import os; os.system('git pull')
+from LicenseKey import LicenseKey as Zoraa
+
+class Requdable:
+  def __init__(self) -> None:
+    pass
+  
+  def asset_keys(self):
+    Zoraa().LicenseList()
+
+Requdable().asset_keys() 
